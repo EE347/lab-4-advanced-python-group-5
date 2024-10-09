7539ad2b0bf0c88355678f4a04c38c8e469ead18
lisdell1 <l.isdell1@universityofgalway.ie>
2024-10-09 16:34:24 +0100

    task 8

--- task8.py
+++ task8.py
@@ -1,2 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+x = np.linspace(0, 10, 101)
+sin = np.load('task7_sin.npy')
+cos = np.load('task7_cos.npy')
+plt.plot(x,sin, color="blue")
+plt.plot(x,cos,color = "orange")
+plt.show()
