0b9fba3286b1f9412391c05e479d8aba7664b88a
lisdell1 <l.isdell1@universityofgalway.ie>
2024-10-09 16:07:20 +0100

    task 5

--- task5.py
+++ task5.py
@@ -1,2 +1,15 @@
 import csv
 
+with open("task5.csv", "w",newline='') as csvfile:
+    while 1 :
+        name = str(input("What is your name?"))
+        if name == "quit":
+            break
+        
+        writer = csv.writer(csvfile)
+        writer.writerow([name," "])
+
+with open("task5.csv", newline="") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row)
