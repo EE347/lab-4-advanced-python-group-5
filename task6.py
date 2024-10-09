641c5bff5022f11925ecff133de59328bc3cbd70
lisdell1 <l.isdell1@universityofgalway.ie>
2024-10-09 16:22:05 +0100

    task 6

--- task6.py
+++ task6.py
@@ -5,6 +5,11 @@ print('Before:')
 print(x)
 
 # Your code goes here
+lenght,height = np.shape(x)
+
+for i in range(1, lenght-1):
+    for j in range(1,height-1):
+        x[i][j] = 0
 
 print('After:') 
 print(x)
